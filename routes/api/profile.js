65625a14919ab9bb72fdb1e733a48d08600054ee
hazique <hazique.q@gmail.com>
2020-09-30 20:53:05 +0530

    Prior to making changes to Profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+con
 
-// @route   GET api/profile
-// @desc Test Route
-// @access Public
-router.get('/', (req, res) => res.send('Profile route'));
+// @route   GET api/profile/me
+// @desc Get Current Users Profile
+// @access private
+router.get('/', auth, async (req, res) => {
+    
+});
 
 module.exports = router;
